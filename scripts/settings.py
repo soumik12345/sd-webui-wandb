dfec9121b5b04b66f876883e24f9fb05ce08c820
soumik12345 <19soumik.rakshit96@gmail.com>
2023-09-15 15:02:19 +0000

    update: wandb settings onchange fn

--- scripts/settings.py
+++ scripts/settings.py
@@ -5,7 +5,8 @@ from modules import shared
 from modules import script_callbacks
 
 
-def login_to_wandb(api_key):
+def login_to_wandb():
+    api_key = shared.opts.wandb_api_key
     shared.log.info("Re-logging to WandB")
     os.environ["WANDB_API_KEY"] = api_key
 
@@ -23,7 +24,7 @@ def wandb_settings():
             component=gr.Textbox,
             component_args={"type": "password"},
             section=section,
-            onchange=login_to_wandb(shared.opts.wandb_api_key),
+            onchange=login_to_wandb(),
         )
     )
 
