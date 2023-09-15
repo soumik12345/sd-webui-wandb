624b665b5fe52309b881d3c969306969d1fb6a12
soumik12345 <19soumik.rakshit96@gmail.com>
2023-09-15 15:20:44 +0000

    update: wandb settings onchange fn

--- scripts/settings.py
+++ scripts/settings.py
@@ -8,8 +8,12 @@ from modules import script_callbacks
 def login_to_wandb():
     try:
         api_key = shared.opts.wandb_api_key
-        shared.log.info("Re-logging in to WandB")
-        os.environ["WANDB_API_KEY"] = api_key
+        if api_key != "":
+            shared.log.info("Re-logging in to WandB")
+            os.environ["WANDB_API_KEY"] = api_key
+            shared.log.info("Successfully set WandB API Key")
+        else:
+            shared.log.error("Unable to re-log in to WandB")
     except AttributeError:
         shared.log.error("Unable to re-log in to WandB")
     except KeyError:
@@ -21,11 +25,11 @@ def wandb_settings():
     shared.opts.add_option(
         "wandb_api_key",
         shared.OptionInfo(
+            default="",
             label="Weights & Biases API Key",
             component=gr.Textbox,
             component_args={"type": "password"},
             section=section,
-            onchange=login_to_wandb(),
             submit=login_to_wandb(),
             comment_before="You can get your WandB API key from [https://wandb.ai/authorize](https://wandb.ai/authorize)"
         )
